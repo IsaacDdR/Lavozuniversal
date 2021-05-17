8bee6b028bc53a93adb704f39aa0efbdd246b3f5
isaacddr <isaacovic00@gmail.com>
2021-05-17 02:20:25 -0500

    Added blog

---
+++ _nuxt/static/1621235917/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621235917",layout:"inicio",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"761f5ede"}}}}(null,"\u002F"));
