b7e4d5ef533d146b8dd3affed3bbaa9416c8aa49
isaacddr <isaacovic00@gmail.com>
2021-05-19 00:10:57 -0500

    Added blog

--- _nuxt/static/1621388839/lavozBlog/musica-mundo/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621388839",layout:"default",error:a,serverRendered:true,routePath:"\u002FlavozBlog\u002Fmusica-mundo",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"58fc30a4"}}}}(null));
