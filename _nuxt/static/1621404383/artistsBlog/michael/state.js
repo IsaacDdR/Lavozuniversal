604eda7390dc9d91904100a94daa05faeecd34d9
isaacddr <isaacovic00@gmail.com>
2021-05-19 01:06:57 -0500

    Added blog

---
+++ _nuxt/static/1621404383/artistsBlog/michael/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621404383",layout:"default",error:a,serverRendered:true,routePath:"\u002FartistsBlog\u002Fmichael",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a387c843"}}}}(null));
