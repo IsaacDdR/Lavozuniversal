0d1a47a162eaa95741713c484a6b7846e0c36a4c
isaacddr <isaacovic00@gmail.com>
2021-06-08 13:13:48 -0500

    Estilizado

--- _nuxt/static/1623172227/artistsBlog/rick/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623172227",layout:"default",error:a,serverRendered:true,routePath:"\u002FartistsBlog\u002Frick",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0c49ae32"}}}}(null));
