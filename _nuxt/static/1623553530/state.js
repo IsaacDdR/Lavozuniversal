a008814532ba170af2f408becf29c73a9fdf822f
isaacddr <isaacovic00@gmail.com>
2021-06-12 23:17:27 -0500

    Estilizado

--- _nuxt/static/1623553530/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623553530",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e101466b"}}}}(null,"\u002F"));
