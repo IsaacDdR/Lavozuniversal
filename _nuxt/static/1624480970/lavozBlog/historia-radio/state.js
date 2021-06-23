86d52d3f890aeef56978764c1074e7aec798ffd6
Isaacddr <isaacovic00@gmail.com>
2021-06-23 15:44:00 -0500

    Added background

---
+++ _nuxt/static/1624480970/lavozBlog/historia-radio/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624480970",layout:"default",error:a,serverRendered:true,routePath:"\u002FlavozBlog\u002Fhistoria-radio",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f3c8a988"}}}}(null));
